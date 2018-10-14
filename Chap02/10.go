bc47694b986fd0c68d12b79ae36adecb32d66138
sato-ken <kengo.carp.no1@gmail.com>
2018-10-14 23:12:29 +0900

    refactoring

--- Chap02/10.go
+++ Chap02/10.go
@@ -6,25 +6,33 @@ import (
 	"os"
 )
 
-func main() {
-	open := func(path string) []string {
-		f, err := os.Open(path)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "File %s could not read: %v\n", path, err)
-		}
-		defer f.Close()
+func readFile(filepath string) ([]string, error) {
 
-		lines := []string{}
-		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			lines = append(lines, scanner.Text())
-		}
-		if serr := scanner.Err(); serr != nil {
-			fmt.Fprintf(os.Stderr, "File %s scan error: %v\n", path, err)
-		}
-		return lines
+	f, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
 	}
-	file := open("../data/hightemp.txt")
-	fmt.Println(len(file))
+	defer f.Close()
+
+	lines := []string{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if serr := scanner.Err(); serr != nil {
+		return nil, serr
+	}
+	return lines, nil
+
+}
+
+func main() {
+
+	file, err := readFile("../data/hightemp.txt")
+	if err == nil {
+		fmt.Println(len(file))
+	} else {
+		fmt.Println(err)
+	}
 
 }
--- Chap02/11.go
+++ Chap02/11.go
@@ -7,27 +7,34 @@ import (
 	"strings"
 )
 
-func main() {
-	open := func(path string) []string {
-		f, err := os.Open(path)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "File %s could not read: %v\n", path, err)
-		}
-		defer f.Close()
+func readFile(filepath string) ([]string, error) {
 
-		lines := []string{}
-		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			lines = append(lines, scanner.Text())
-		}
-		if serr := scanner.Err(); serr != nil {
-			fmt.Fprintf(os.Stderr, "File %s scan error: %v\n", path, err)
-		}
-		return lines
+	f, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
 	}
+	defer f.Close()
 
-	for _, v := range open("../data/hightemp.txt") {
-		fmt.Println(strings.Replace(v, "\t", " ", -1))
+	lines := []string{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if serr := scanner.Err(); serr != nil {
+		return nil, serr
+	}
+	return lines, nil
+
+}
+
+func main() {
+
+	v, err := readFile("../data/hightemp.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, line := range v {
+		fmt.Println(strings.Replace(line, "\t", " ", -1))
 	}
 
 }
